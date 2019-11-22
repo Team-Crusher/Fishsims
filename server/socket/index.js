892fdac3cee0f33c295e0e4bbd77d896a8bda2ad
Chris Kang <christopha.kang@gmail.com>
2019-11-22 15:19:18 -0600

    initial sockets

--- server/socket/index.js
+++ server/socket/index.js
@@ -2,6 +2,16 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    socket.on('new-player', socketId => {
+      // make new player
+      console.log('socket id: ', socketId)
+      socket.emit('player-init', {
+        socketId,
+        boats: [],
+        fisheries: [{x: 20, y: 20}]
+      })
+    })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
