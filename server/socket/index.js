9baa5ad950b3271e8cc0d0626260ae1e8a162895
Chris Kang <christopha.kang@gmail.com>
2019-11-23 12:52:18 -0600

    fixed issue with gamestate not rendering on initial arrival at page

--- server/socket/index.js
+++ server/socket/index.js
@@ -7,11 +7,17 @@ module.exports = io => {
     socket.on('new-player', socketId => {
       // make new player
       console.log('socket id: ', socketId)
+
+      // TO DO: set up a new Player object on the server side
+      // store socket ID and assign a color
+
       socket.emit('player-init', {
         socketId,
         boats: [],
         fisheries: [{x: 20, y: 20}]
       })
+
+      io.emit('server-circles', circles)
     })
 
     socket.on('circle-add', circle => {
