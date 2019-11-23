dce78d2b52ad7f5bd73654adf49d551dbd88aab6
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-23 12:59:11 -0600

    ping client every 5 seconds to prevent disconnection

--- server/socket/index.js
+++ server/socket/index.js
@@ -4,6 +4,11 @@ module.exports = io => {
   io.on('connection', socket => {
     console.log(`A socket connection to the server has been made: ${socket.id}`)
 
+    // Keep pinging all clients, so that they don't disconnect mid-game
+    setInterval(() => {
+      io.emit('ping', 'ping')
+    }, 5000)
+
     socket.on('new-player', socketId => {
       // make new player
       console.log('socket id: ', socketId)
@@ -13,10 +18,14 @@ module.exports = io => {
         fisheries: [{x: 20, y: 20}]
       })
     })
+
+    socket.on('pong', sid => console.log('pong from: ', sid))
+
     socket.on('circle-add', circle => {
       circles.push(circle)
       io.emit('server-circles', circles)
     })
+
     socket.on('disconnect', () => {
       console.log(`Connection ${socket.id} has left the building`)
     })
