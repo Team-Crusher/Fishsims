5ba583603c1b96a8e6cd90b3857394c7531e331a
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-27 13:34:58 -0600

    pass in resources and spritePath as params instead of importing them

--- client/store/fish.js
+++ client/store/fish.js
@@ -1,12 +1,9 @@
 import {Sprite} from 'pixi.js'
-import {resources, spritePath} from '../script/game.js'
-/**
- * ACTION TYPES
- */
+
 const SET_FISHES = 'SET_FISHES'
 const REMOVE_FISH = 'REMOVE_FISH' // for client-side rendering
 
-export const setFishes = fishes => {
+export const setFishes = (fishes, resources, spritePath) => {
   const clientFishes = fishes.map(fish => {
     if (!fish.sprite)
       return {
@@ -21,12 +18,13 @@ export const setFishes = fishes => {
   }
 }
 
-export default function(state = [], action) {
+const reducer = (state = [], action) => {
   switch (action.type) {
     case SET_FISHES:
-      console.log('here')
       return action.fishes
     default:
       return state
   }
 }
+
+export default reducer
