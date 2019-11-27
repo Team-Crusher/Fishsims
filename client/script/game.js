46cb338263f63e76e0952beb2c90c01e3ac332d0
Charlie <achanont@gmail.com>
2019-11-27 16:10:51 -0600

    fisheries moving works

--- client/script/game.js
+++ client/script/game.js
@@ -111,9 +111,44 @@ function setup() {
     )
     fisherySprites.position.set(fishery.x * TILE_SIZE, fishery.y * TILE_SIZE)
     fisherySprites.socketId = fishery.socketId
+    fisherySprites.interactive = true
+    fisherySprites.buttonMode = true
+    fisherySprites.anchor.set(0.5)
+    fisherySprites
+      .on('pointerdown', onDragStart)
+      .on('pointerup', onDragEnd)
+      .on('pointerupoutside', onDragEnd)
+      .on('pointermove', onDragMove)
+
+    // For mouse-only events
+    // .on('mousedown', onDragStart)
+
     app.stage.addChild(fisherySprites)
     return fisherySprites
   })
+  function onDragStart(event) {
+    // store a reference to the data
+    // the reason for this is because of multitouch
+    // we want to track the movement of this particular touch
+    this.data = event.data
+    this.alpha = 0.5
+    this.dragging = true
+  }
+
+  function onDragEnd() {
+    this.alpha = 1
+    this.dragging = false
+    // set the interaction data to null
+    this.data = null
+  }
+
+  function onDragMove() {
+    if (this.dragging) {
+      const newPosition = this.data.getLocalPosition(this.parent)
+      this.x = newPosition.x
+      this.y = newPosition.y
+    }
+  }
 
   // add a menu child to the app.stage
   // make menu a container
