d9b1b3d38f3a05d84221e24dc4f482c1ec6c7285
njasi <nick@jasinski3.com>
2019-11-28 13:36:43 -0600

    cleans up sockets for others to easily use

---
+++ client/socket/game.js
@@ -0,0 +1,17 @@
+// put any game socket listening in here
+
+export default socket => {
+  // whenever the server sends the game state
+  // socket.on('send-game-state', gameState => {
+  //   // get map
+  //   store.dispatch(setMap(gameState.board))
+  //   // get fish
+  //   store.dispatch(setFish(gameState.fish))
+  //   // get boats
+  //   store.dispatch(
+  //     setBoats(
+  //       gameState.players.reduce((acc, player) => acc.concat(player.boats), [])
+  //     )
+  //   )
+  // })
+}
--- client/socket/lobby.js
+++ client/socket/lobby.js
@@ -1,16 +1,28 @@
-import store, {setPlayers, addPlayer, removePlayer, setLobbyId} from '../store'
+import store, {
+  setPlayers,
+  addPlayer,
+  removePlayer,
+  setLobbyId,
+  setRoute
+} from '../store'
+
+import gameSockets from './game'
+import chatSockets from './chat'
 
 export default (socket, data) => {
   switch (data.status) {
-    case 200:
-    case 201:
-      store.dispatch(setPlayers(data.players))
+    case 200: // added to lobby
+    case 201: // last person to make it into lobby (no diff right now)
       break
     default:
+      // TODO failed to add to lobby (add stuff here later lik lobby full whn you join by link etc)
       return
   }
 
-  store.dispatch(setLobbyId(data.lobbyId))
+  // all of the below only happens if the person is actually in a lobby
+
+  store.dispatch(setLobbyId(data.lobbyId)) // sets the lobbyId if it is ever neeeded
+  store.dispatch(setPlayers(data.players)) // sets init players in lobby
 
   socket.on('player-added-to-lobby', player => {
     store.dispatch(addPlayer(player))
@@ -18,5 +30,25 @@ export default (socket, data) => {
 
   socket.on('player-left-lobby', player => {
     store.dispatch(removePlayer(player))
+  })
+
+  socket.on('vote-to-start-early', player => {
+    // TODO show vote next to player who votes to start early
+  })
+
+  socket.on('retract-vote-to-start-early', player => {
+    // TODO remove vote next to player who retracts their vote
+  })
+
+  socket.on('game-cancel', () => {
+    // TODO leave the lobby (add serverside to change status back etc...)
+    store.dispatch(setRoute('HOME'))
+    store.dispatch(setLobbyId(null))
+  })
+
+  socket.on('game-start', () => {
+    store.dispatch(setRoute('GAME')) // switches the view to game
+    gameSockets(socket) // attaches game listeners
+    chatSockets(socket) // attaches chat listeners
   })
 }
