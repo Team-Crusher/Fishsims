e51cf517f85e87e14bef6b39127909718f2466eb
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-11-29 13:43:54 -0600

    wip serving up map to front- from back-end

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,27 +1,6 @@
 const lobbies = require('../lobbyer')
 const lobbySockets = require('./lobby')
 
-// TODO: move below code to game.js or make a map.js or something
-// make new map and make sure that it's viable
-// TODO: output image
-
-const {spawnDock, getLand, getWater} = require('../../utilityMethods.js')
-const {makeMap} = require('../../fractal-noise.js')
-const {TILE_SIZE} = require('../../client/script/drawMap.js')
-
-let newMap = makeMap()
-let landTiles = getLand(newMap)
-let waterTiles = getWater(newMap)
-while (
-  landTiles.length > waterTiles.length ||
-  landTiles.length < TILE_SIZE * 50
-) {
-  newMap = makeMap()
-  landTiles = getLand(newMap)
-  waterTiles = getWater(newMap)
-}
-
-//
 module.exports = io => {
   io.on('connection', socket => {
     console.log(`NEW SOCKET CONNECTION: ${socket.id}`)
