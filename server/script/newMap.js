97af095c1cb8e42f622e18c222c6c7a1f90d16df
njasi <nick@jasinski3.com>
2019-11-29 16:45:42 -0600

    landTiles prop

--- server/script/newMap.js
+++ server/script/newMap.js
@@ -1,19 +1,15 @@
 const {getLand, getWater} = require('../../utilityMethods.js')
-const {makeMap} = require('../../fractal-noise.js')
+const {makeMap, N} = require('../../fractal-noise.js')
 const {TILE_SIZE} = require('../../client/script/drawMap.js')
 
 // make new map and make sure that it's viable
 module.exports = () => {
-  let newMap = makeMap()
-  let landTiles = getLand(newMap)
-  let waterTiles = getWater(newMap)
-  while (
-    landTiles.length > waterTiles.length ||
-    landTiles.length < TILE_SIZE * 50
-  ) {
+  const area = N * N,
+    propLand = 0.3
+  let newMap, landTiles
+  do {
     newMap = makeMap()
     landTiles = getLand(newMap)
-    waterTiles = getWater(newMap)
-  }
+  } while (landTiles.length / area > propLand)
   return newMap
 }
