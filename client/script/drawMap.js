8c07dcf5b8110eeaa3ac7e5941df6688a5a60d08
njasi <nick@jasinski3.com>
2019-12-01 17:58:16 -0600

    map gets added to pixi

--- client/script/drawMap.js
+++ client/script/drawMap.js
@@ -1,9 +1,9 @@
 const TILE_SIZE = 32
 const SEA_LEVEL = 47
+const PIXEL = 1
 
 const drawMap = (ctx, map) => {
   //    ctx.scale(0.6, 0.6)
-  ctx.scale(1, 1)
   map.forEach((row, i) => {
     row.forEach((x, j) => {
       if (x >= 60) ctx.fillStyle = 'silver'
@@ -13,11 +13,12 @@ const drawMap = (ctx, map) => {
       else if (x < 47 && x >= 30) ctx.fillStyle = 'deepskyblue'
       else if (x < 30 && x >= 15) ctx.fillStyle = 'royalblue'
       else ctx.fillStyle = 'mediumblue'
-      ctx.fillRect(TILE_SIZE * j, TILE_SIZE * i, TILE_SIZE, TILE_SIZE)
+      ctx.fillRect(PIXEL * j, PIXEL * i, PIXEL, PIXEL)
       ctx.strokeStyle = ctx.fillStyle
-      ctx.strokeRect(TILE_SIZE * j, TILE_SIZE * i, TILE_SIZE, TILE_SIZE)
+      ctx.strokeRect(PIXEL * j, PIXEL * i, PIXEL, PIXEL)
     })
   })
+  return ctx.canvas.toDataURL()
 }
 
 module.exports = {drawMap, TILE_SIZE, SEA_LEVEL}
