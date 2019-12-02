4ffaf7ff3ad709151b775f55972d190d885e329f
njasi <nick@jasinski3.com>
2019-12-01 18:07:54 -0600

    blur?

--- client/script/makeMapSprite.js
+++ client/script/makeMapSprite.js
@@ -3,7 +3,8 @@ import {resources} from './game'
 
 const makeMapSprite = () => {
   const map = new Sprite(resources.map.texture)
-  map.texture.baseTexture.scaleMode = SCALE_MODES.NEAREST
+  console.log(SCALE_MODES)
+  map.texture.baseTexture.scaleMode = 0
   map.scale.set(32, 32)
   return map
 }
