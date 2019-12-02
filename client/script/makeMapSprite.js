1806146498c90b221cac7f867b156af9314ea78e
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-12-02 17:55:29 -0600

    wip getting actual map coordinates...

--- client/script/makeMapSprite.js
+++ client/script/makeMapSprite.js
@@ -1,6 +1,7 @@
 import {Sprite, SCALE_MODES} from 'pixi.js'
 import {resources} from './game'
 import store, {setEnd} from '../store'
+import {TILE_SIZE} from './drawMap.js'
 
 const makeMapSprite = () => {
   const map = new Sprite(resources.map.texture)
@@ -8,11 +9,15 @@ const makeMapSprite = () => {
   map.texture.baseTexture.scaleMode = SCALE_MODES.NEAREST
   map.scale.set(32, 32)
   map.interactive = true
-  map.on('pointerdown', () => {
-    if (store.getState().pathfinding.start) console.log('hi')
-    else console.log('bye')
-  })
+  map.on('click', handleClick)
   return map
+}
+
+const handleClick = e => {
+  if (store.getState().pf.start.row) {
+    //    console.log('col: ', e.data.global.x / TILE_SIZE, 'row: ', e.data.global.y / TILE_SIZE)
+    console.log(e.data)
+  } else console.log('bye')
 }
 
 export default makeMapSprite
