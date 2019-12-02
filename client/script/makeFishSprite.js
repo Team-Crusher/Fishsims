28a395d27022d0222f6b3857660e623265a80f78
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-12-02 01:18:33 -0600

    sets anchor, scale, position, and quantity of fish

--- client/script/makeFishSprite.js
+++ client/script/makeFishSprite.js
@@ -4,10 +4,12 @@ import {TILE_SIZE} from '../script/drawMap'
 
 const makeFishSprite = fish => {
   const sprite = new Sprite(resources[fishesImage].texture)
-  sprite.position.set(fish.x * TILE_SIZE, fish.y * TILE_SIZE)
-  sprite.quantity = fish.pop // TODO: divide by... 10? 50? 100?
+  sprite.position.set(fish.col * TILE_SIZE, fish.row * TILE_SIZE)
+  sprite.quantity = 100
+  sprite.anchor.set(0.5)
+  sprite.scale.set(2)
   stage.addChild(sprite)
-  const nameText = new Text(fish.pop, {
+  /*  const nameText = new Text(sprite.quantity, {
     fontFamily: 'Arial',
     fontSize: 12, // TODO: remove once tiles correspond to population
     fill: 'white',
@@ -15,7 +17,7 @@ const makeFishSprite = fish => {
   })
 
   sprite.addChild(nameText)
-  nameText.y += 24
+  nameText.y += 24*/
 
   return sprite
 }
