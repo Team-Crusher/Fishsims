626e104626b005dcec4675f3cd5b9b52ad54650a
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-12-02 17:07:47 -0600

    click handler says 'hi' or 'bye' depending on whether a boat is selected

--- client/script/makeMapSprite.js
+++ client/script/makeMapSprite.js
@@ -1,11 +1,17 @@
 import {Sprite, SCALE_MODES} from 'pixi.js'
 import {resources} from './game'
+import store, {setEnd} from '../store'
 
 const makeMapSprite = () => {
   const map = new Sprite(resources.map.texture)
   map.zIndex = -9001
   map.texture.baseTexture.scaleMode = SCALE_MODES.NEAREST
   map.scale.set(32, 32)
+  map.interactive = true
+  map.on('pointerdown', () => {
+    if (store.getState().pathfinding.start) console.log('hi')
+    else console.log('bye')
+  })
   return map
 }
 
