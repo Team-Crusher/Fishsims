0fc7c0087bbdba4094254a09b68c262f5afd8a59
Frances Tiffin <ftiffin@alumnae.mills.edu>
2019-12-02 15:52:04 -0600

    add setStart to be set when a boat is clicked

--- client/store/pfGrid.js
+++ client/store/pfGrid.js
@@ -1,10 +1,19 @@
 const SET_PF_GRID = 'SET_PF_GRID'
+const SET_START = 'SET_START'
+const SET_END = 'SET_END'
+
 export const setPFGrid = pfGrid => ({type: SET_PF_GRID, pfGrid})
+export const setStart = start => ({type: SET_START, start})
+export const setEnd = end => ({type: SET_START, end})
 
 export default function(state = [], action) {
   switch (action.type) {
     case SET_PF_GRID:
       return action.pfGrid
+    case SET_START:
+      return action.start
+    case SET_END:
+      return action.end
     default:
       return state
   }
