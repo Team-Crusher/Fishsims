e2c0d91b4b33ecf423ef883f697b77b0cd277d1f
njasi <nick@jasinski3.com>
2019-12-05 13:31:42 -0600

    various bug fixes

--- server/socket/game.js
+++ server/socket/game.js
@@ -75,7 +75,7 @@ const gameSockets = (socket, io) => {
     ) {
       io
         .in(lobby.id)
-        .emit('start-server-turn', lobStore.getState().serverActionsReel) //TODO go to client to fix this
+        .emit('start-server-turn', lobStore.getState().serverActionsReel) // TODO go to client to fix this
       clearInterval(lobby.interval)
       lobStore.dispatch(resetEndTurns())
       lobStore.dispatch(resetReel())
