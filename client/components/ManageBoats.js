1d7e62fff03b0d4257c28d349a14b5ed5aad5050
Chris Kang <christopha.kang@gmail.com>
2019-12-17 17:29:56 -0600

    clicking a boat in the listing also selects and preps it for moving. good stopping point.

--- client/components/ManageBoats.js
+++ client/components/ManageBoats.js
@@ -2,11 +2,15 @@ import React from 'react'
 import {connect} from 'react-redux'
 import socket from '../socket'
 import {getBoatFishTotal} from '../script/utils'
+import {clearRange, makeBoatHighlight} from '../script/sprites'
 import FlipMove from 'react-flip-move'
+import {viewport} from '../script/game'
+import {setSelectedObject, setStart} from '../store'
+import {TILE_SIZE} from '../../server/CONSTANTS'
 
 class ManageBoats extends React.Component {
   render() {
-    const {boats, me} = this.props
+    const {boats, me, setBoatAsSelected, setPathStart} = this.props
     const myBoats = boats.filter(b => b.ownerSocket === socket.id)
 
     return (
@@ -24,7 +28,19 @@ class ManageBoats extends React.Component {
               return 1
             })
             .map(b => (
-              <div key={b.id} id="manage-boat">
+              <div
+                key={b.id}
+                id="manage-boat"
+                onClick={() => {
+                  viewport.moveCenter(b.x, b.y)
+
+                  //select the boat and prepare to move it
+                  clearRange()
+                  setBoatAsSelected(b)
+                  setPathStart({row: b.y / TILE_SIZE, col: b.x / TILE_SIZE})
+                  makeBoatHighlight(b.x, b.y, b.maxDistance, b.sprite)
+                }}
+              >
                 <div>
                   <div style={{color: me.color}}>{b.name}</div>
                   {b.status === 'Idle' ? (
@@ -58,4 +74,11 @@ const mapState = state => {
   }
 }
 
-export default connect(mapState)(ManageBoats)
+const mapDispatch = dispatch => {
+  return {
+    setBoatAsSelected: boat => dispatch(setSelectedObject(boat)),
+    setPathStart: coordsObj => dispatch(setStart(coordsObj))
+  }
+}
+
+export default connect(mapState, mapDispatch)(ManageBoats)
